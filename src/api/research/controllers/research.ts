dd080aea27015fba018fbe0391354dfa2dbc4d0b
Yiming <ymjrcc@qq.com>
2025-08-01 18:03:21 +0800

    feat: add homepage and researches

---
+++ src/api/research/controllers/research.ts
@@ -0,0 +1,7 @@
+/**
+ * research controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::research.research');
---
+++ src/api/research/routes/research.ts
@@ -0,0 +1,7 @@
+/**
+ * research router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::research.research');
